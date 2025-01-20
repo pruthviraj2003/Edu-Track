f7dc333aee284f961ea6593cc63d9bd5c7158852
Pruthviraj Suryawanshi <pruthvieaj1@gmail.com>
2025-01-20 08:36:52 +0530

    Welcome Page UI

--- edu_track/lib/main.dart
+++ edu_track/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:edu_track/view/signup_screen.dart';
+import 'package:edu_track/view/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SignupScreen(),
+      debugShowCheckedModeBanner: false,
+      home: Welcome(),
     );
   }
 }
---
+++ edu_track/lib/view/welcome_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            //welcome Image
+            SizedBox(
+              height: 150,
+            ),
+            Image.asset('lib/assets/welcomeImage.png'),
+            SizedBox(
+              height: 20,
+            ),
+            //Title
+            Text(
+              "EduTrack",
+              style: GoogleFonts.poppins(
+                  fontSize: 36, fontWeight: FontWeight.w800),
+            ),
+            //Button
+            const SizedBox(
+              height: 60,
+            ),
+            SizedBox(
+              height: 65,
+              width: 267,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Text(
+                    "Sign In",
+                    style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  )),
+            ),
+            //sub text
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "don’t have an account?",
+                  style: GoogleFonts.poppins(),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Sign Up",
+                    style: GoogleFonts.poppins(fontWeight: FontWeight.w600),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
